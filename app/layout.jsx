e9f78bc115cef8161a3da57ae4bedf653c4379e0
Mano Vikram <manovik18@gmail.com>
2025-12-24 01:00:41 +0530

    Added the necessary base fonts

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,23 @@
+import Navbar from "@/components/navbar";
 import "./globals.css";
+import { Montserrat_Alternates, Philosopher } from "next/font/google"
 
 export const metadata = {
   title: "AI Research Assistant",
   description: "An AI-powered research assistant to help you with your research tasks.",
 };
 
+const philosopherFont = Philosopher({ subsets: ["latin"], weight: ["400", "700"], variable: "--font-philosopher" });
+const montserratAlternatesFont = Montserrat_Alternates({ subsets: ["latin"], weight: ["400", "700"], variable: "--font-montserrat-alternates" });
+
 // Design inspiration - https://dribbble.com/shots/26403687-AI-Landing-Page-AI-Assistant-Wellness-AI-Chatbot-UIUX
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`antialiased`}>
+    <html lang="en" className={`${philosopherFont.variable} ${montserratAlternatesFont.variable}`}>
+      <body className={`antialiased min-h-dvh w-full`}>
+        <Navbar />
+
         {children}
       </body>
     </html>

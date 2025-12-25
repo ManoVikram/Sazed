2db7b56375e51c3106ff489a10f50131b9fee63b
Mano Vikram <manovik18@gmail.com>
2025-12-26 00:13:39 +0530

    Updated the UI to display the research summary and critique returned by the backend

--- app/research/page.jsx
+++ app/research/page.jsx
@@ -4,33 +4,64 @@ import { Spinner } from '@/components/ui/spinner'
 import * as helpers from '@/lib/api/helpers'
 import Image from 'next/image'
 import React, { useState } from 'react'
+import ReactMarkdown from 'react-markdown';
 
 const ResearchPage = () => {
   const [query, setQuery] = useState("")
   const [isLoading, setIsLoading] = useState(false)
+  const [researchData, setResearchData] = useState(null)
 
   const handleQuerySubmit = async () => {
     setIsLoading(true)
     const response = await helpers.research(query)
     console.log(response)
+    setResearchData(response)
     setIsLoading(false)
   }
 
   return (
-    <section className='flex flex-col flex-1 justify-center items-center'>
-      <div className="flex flex-col justify-center items-center w-full gap-8">
-        <div className='flex flex-col justify-center items-center gap-5'>
-          <h1 className='text-7xl'>Hello, Researcher</h1>
+    <section className='flex flex-col flex-1 justify-center items-center p-6'>
+      <div className="flex flex-col flex-1 justify-center items-center w-2/3 gap-6">
+        {researchData ? (
+          <div className="flex flex-row flex-1 w-full mt-16 gap-3">
+            <div className="flex flex-1 flex-col rounded-2xl p-3 border-2 border-gray-300 gap-4">
+              <div className="flex justify-center items-start w-full">
+                <h3 className='text-2xl'>Research Summary</h3>
+              </div>
 
-          <div className="flex flex-col justify-center items-center gap-2">
+              <p className='font-montserrat-alternates text-gray-500 text-sm'>
+                <ReactMarkdown>
+                  {researchData.summary}
+                </ReactMarkdown>
+              </p>
+            </div>
 
-            <h2 className='text-5xl font-montserrat-alternates font-bold text-gray-400'>Let&apos;s make your research easier.</h2>
+            <div className="flex flex-1 flex-col rounded-2xl p-2 border-2 border-gray-300 gap-4">
+              <div className="flex justify-center items-start w-full">
+                <h3 className='text-2xl'>Research Critique</h3>
+              </div>
 
-            <p className='text-lg font-montserrat-alternates text-gray-400'>Your personal AI assistant for research and knowledge.</p>
+              <p className='font-montserrat-alternates text-gray-500 text-sm'>
+                <ReactMarkdown>
+                  {researchData.critique}
+                </ReactMarkdown>
+              </p>
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className='flex flex-col justify-center items-center gap-5'>
+            <h1 className='text-7xl'>Hello, Researcher</h1>
 
-        <div className="flex flex-col justify-start items-start w-2/3 px-6 py-3 bg-amber-50/80 rounded-3xl gap-6">
+            <div className="flex flex-col justify-center items-center gap-2">
+
+              <h2 className='text-5xl font-montserrat-alternates font-bold text-gray-400'>Let&apos;s make your research easier.</h2>
+
+              <p className='text-lg font-montserrat-alternates text-gray-400'>Your personal AI assistant for research and knowledge.</p>
+            </div>
+          </div>
+        )}
+
+        <div className="flex flex-col justify-start items-start w-full px-6 py-3 bg-amber-50/80 rounded-3xl gap-6">
           <input type="text" className='w-full text-lg outline-none ring-0 border-none' placeholder='Ask anything...' value={query} onChange={(event) => setQuery(event.target.value)} />
 
           <div className="flex justify-between items-center w-full">
